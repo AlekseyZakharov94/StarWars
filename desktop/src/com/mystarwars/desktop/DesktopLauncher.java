7babcfe77a8ae9bc0cca51d1cc0e0c61ae733bbf
Aleksey_Zakharov <a.zakharov@yandex.ru>
2020-10-15 09:31:19 +0300

    Initial commit

---
+++ desktop/src/com/mystarwars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mystarwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mystarwars.StarWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarWars(), config);
+	}
+}
